0c492f45e01bb0f17758f1d74b72c311a03173e2
gulhayotemirova <117151596+gulhayotemirova@users.noreply.github.com>
2022-12-14 17:53:12 +0500

    Add files via upload

---
+++ 2-amaliy1.py
@@ -0,0 +1,11 @@
+import math
+musbat=int(input("musbat son kiriting: "))
+y=musbat%2
+if musbat>99 and musbat<1000:
+    print("bu son uch xonali")
+else:
+    print("bu son uch xonali emas")
+if y==1:
+    print("bu son toq")
+else:
+    print("bu son toq emas")
