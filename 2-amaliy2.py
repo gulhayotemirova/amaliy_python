5def1c5c4693e19ffcf8ce229e99612838ac40f6
gulhayotemirova <117151596+gulhayotemirova@users.noreply.github.com>
2022-12-14 17:54:18 +0500

    Add files via upload

---
+++ 2-amaliy2.py
@@ -0,0 +1,8 @@
+import math
+A=int(input("A son kiriting: "))
+N=int(input("N-darajani  kiriting: "))
+y=A**N
+if N>0:
+    print("A ning N inchi darajasi:",y)
+else:
+    print("N ga musbat son kiriting!!!")
