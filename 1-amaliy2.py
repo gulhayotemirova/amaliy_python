90dce89889919d0d21f448cf6d930635359f5b3c
gulhayotemirova <117151596+gulhayotemirova@users.noreply.github.com>
2022-12-14 17:51:49 +0500

    Add files via upload

---
+++ 1-amaliy2.py
@@ -0,0 +1,6 @@
+import math
+kk=int(input("kelgan kunini kiriting: "))
+bir_kunlik=70000/22
+sum=kk*bir_kunlik
+print("Bir kunlik puli:", bir_kunlik.__round__())
+print("To'lash kerak: ", sum.__round__())
